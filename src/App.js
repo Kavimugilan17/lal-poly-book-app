c607c7050a0fd03bd2ad1e1a540c8b951039a79d
Kavimugilan <58725968+Kavimugilan17@users.noreply.github.com>
2020-09-17 14:15:58 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Appbar from "./components/Appbar";
+import SideBar from "./components/SideBar";
+
+function App() {
+  return (
+    <div className="App">
+      <Appbar />
+      <SideBar />
+    </div>
+  );
+}
+
+export default App;
